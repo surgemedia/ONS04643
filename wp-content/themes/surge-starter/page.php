86208031e31f7f1bb28e9fe4f5bb59f1753eefe1
Hector <hectornucv@gmail.com>
2016-04-21 11:10:01 +1000

    Wordpress instalation

---
+++ wp-content/themes/surge-starter/page.php
@@ -0,0 +1,4 @@
+<?php while (have_posts()) : the_post(); ?>
+  <?php get_template_part('components/molecule/page', 'header'); ?>
+  <?php get_template_part('components/molecule/content', 'page'); ?>
+<?php endwhile; ?>
