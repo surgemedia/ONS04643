bc8133d291de1eeab26d74474d0c1da2dab18ce7
Hector <hectornucv@gmail.com>
2016-05-12 09:18:44 +1000

    Form Section Contact Us

--- wp-content/themes/surge-starter/components/organism/contact-us.php
+++ wp-content/themes/surge-starter/components/organism/contact-us.php
@@ -3,7 +3,7 @@
 
  ?>
 
-<section id="contact-us" class="<?php echo $vars["class"] ?>" style="background-image:url(<?php echo $image ?>)">
+<section id="contact-us" class="<?php echo $vars['class']?>" style="background-image: url('<?php echo $image;?>');">
 		<div class="container white-font text-center">
 		<?php displayGravityForm(get_field("contact_us_form","option"),false,false,false,true,0); ?>
 		</div>
--- wp-content/themes/surge-starter/template-TID02.php
+++ wp-content/themes/surge-starter/template-TID02.php
@@ -12,16 +12,16 @@
 			= @components
 				+ molecule/card-header
 			=============================================*/
-			// get_component([ 'template' => 'molecule/card-header',
-			// 								// 'remove_tags' =>  ['i'],
-			// 								'vars' => [
-			// 											"class" => 'col-md-12',
-			// 											"image" => 'http://localhost/ONS04643/wp-content/uploads/2016/05/any1-1.jpg',
-			// 											"subtitle" => "Onsite for the mining industry",
-			// 											"title" => "One stop power shop",
-			// 											"content" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe sed odit inventore a quo mollitia adipisci est placeat omnis earum quisquam minus sit iusto, accusamus et facere. Eligendi distinctio neque delectus natus, doloribus animi velit pariatur, minus vero. Dolores quis asperiores sequi maiores, doloribus, cum quia reprehenderit accusantium accusamus at.",
-			// 											]
-			// 								 ]);
+			get_component([ 'template' => 'molecule/card-header',
+											// 'remove_tags' =>  ['i'],
+											'vars' => [
+														"class" => 'col-md-12',
+														"image" => 'http://localhost/ONS04643/wp-content/uploads/2016/05/any1-1.jpg',
+														"subtitle" => "Onsite for the mining industry",
+														"title" => "One stop power shop",
+														"content" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe sed odit inventore a quo mollitia adipisci est placeat omnis earum quisquam minus sit iusto, accusamus et facere. Eligendi distinctio neque delectus natus, doloribus animi velit pariatur, minus vero. Dolores quis asperiores sequi maiores, doloribus, cum quia reprehenderit accusantium accusamus at.",
+														]
+											 ]);
 	 ?>
 
 <?php
@@ -30,28 +30,28 @@
 			= @components
 				+ molecule/card
 			=============================================*/
-			// get_component([ 'template' => 'molecule/card',
-			// 								// 'remove_tags' =>  ['i'],
-			// 								'vars' => [
-			// 											"class" => 'col-md-6',
-			// 											"image" => 'http://localhost/ONS04643/wp-content/uploads/2016/05/any4-1.jpg',
-			// 											"title" => "One stop power shop",
-			// 											"content" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe sed odit inventore a quo mollitia adipisci est placeat omnis earum quisquam minus sit iusto, accusamus et facere. Eligendi distinctio neque delectus natus, doloribus animi velit pariatur, minus vero. Dolores quis asperiores sequi maiores, doloribus, cum quia reprehenderit accusantium accusamus at.",
-			// 											]
-			// 								 ]);
+			get_component([ 'template' => 'molecule/card',
+											// 'remove_tags' =>  ['i'],
+											'vars' => [
+														"class" => 'col-md-6',
+														"image" => 'http://localhost/ONS04643/wp-content/uploads/2016/05/any4-1.jpg',
+														"title" => "One stop power shop",
+														"content" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe sed odit inventore a quo mollitia adipisci est placeat omnis earum quisquam minus sit iusto, accusamus et facere. Eligendi distinctio neque delectus natus, doloribus animi velit pariatur, minus vero. Dolores quis asperiores sequi maiores, doloribus, cum quia reprehenderit accusantium accusamus at.",
+														]
+											 ]);
 	 ?>
 <?php
 
 
-			// get_component([
-			// 								'template' => 'atom/link',
-			// 								// 'return_string' => true,
-			// 								'vars' => [
-			// 											"class" => 'button text-uppercase',
-			// 											"text" => 'Read More',
-			// 											"url" => "get_permalink()"
-			// 											]
-			// 							]);
+			get_component([
+											'template' => 'atom/link',
+											// 'return_string' => true,
+											'vars' => [
+														"class" => 'button text-uppercase',
+														"text" => 'Read More',
+														"url" => "get_permalink()"
+														]
+										]);
 		 ?>
 
 
