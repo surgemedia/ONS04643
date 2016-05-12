c3a55cc59e23caf046788ddf34af065b15c01f01
Alex <alexanderddking@gmail.com>
2016-05-12 14:10:26 +1000

    Default Card

--- wp-content/themes/surge-starter/components/molecule/card.php
+++ wp-content/themes/surge-starter/components/molecule/card.php
@@ -1,8 +1,9 @@
 <article class="<?php echo $vars['class'] ?>">
-	<img class="img-responsive" src="<?php echo $vars["image"]?>" alt="">
+	<img class="img-responsive" src="<?php echo $vars["image"]?>" alt=""></img>
 	<hgroup>
 		<h6><?php echo $vars["subtitle"]?></h6>
 		<h1><?php echo $vars["title"]?></h1>
 	</hroup>
 	<p><?php echo $vars["content"] ?></p>
+	<?php echo $vars['button']; ?>
 </article>
